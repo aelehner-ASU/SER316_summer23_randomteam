bf255bc3cb3f62862ae73970962bdae9e1627822
aelehner-ASU <101679651+aelehner-ASU@users.noreply.github.com>
2023-07-09 01:12:42 -0500

    Add Belt Ranks to UserAccount.java

--- src/main/java/memoranda/ui/UserAccount.java
+++ src/main/java/memoranda/ui/UserAccount.java
@@ -16,26 +16,44 @@ public class UserAccount {
 	private String username, password;
 	private enum AcctType {TRAINER, STUDENT};
 	private AcctType acctType;
+	private enum BeltRank {WHITE, YELLOW, ORANGE, PURPLE, BLUE, BLUESTRIPE, GREEN, GREENSTRIPE,
+		BROWN1, BROWN2, BLACK1, BLACK2, BLACK3};
+	private BeltRank beltRank;
 	private static String file = "UserAccounts.txt";
 	
 	/**
 	 * Constructor
 	 * @param inUsername
 	 * @param inPassword
-	 * @param inType
+	 * @param inAcctType
+	 * @param beltType
 	 */
-	public UserAccount(String inUsername, String inPassword, int inType) throws IOException {
+	public UserAccount(String inUsername, String inPassword, int inAcctType, int beltType) throws IOException {
 		username = inUsername;
 		password = inPassword;
-		if (inType == 0) {acctType = AcctType.TRAINER;}
-		if (inType == 1) {acctType = AcctType.STUDENT;}	
+		if (inAcctType == 0) {acctType = AcctType.TRAINER;}
+		if (inAcctType == 1) {acctType = AcctType.STUDENT;}	
+		if (beltType == 0) {beltRank = BeltRank.WHITE;}
+		if (beltType == 1) {beltRank = BeltRank.YELLOW;}
+		if (beltType == 2) {beltRank = BeltRank.ORANGE;}
+		if (beltType == 3) {beltRank = BeltRank.PURPLE;}
+		if (beltType == 4) {beltRank = BeltRank.BLUE;}
+		if (beltType == 5) {beltRank = BeltRank.BLUESTRIPE;}
+		if (beltType == 6) {beltRank = BeltRank.GREEN;}
+		if (beltType == 7) {beltRank = BeltRank.GREENSTRIPE;}
+		if (beltType == 8) {beltRank = BeltRank.BROWN1;}
+		if (beltType == 9) {beltRank = BeltRank.BROWN2;}
+		if (beltType == 10) {beltRank = BeltRank.BLACK1;}
+		if (beltType == 11) {beltRank = BeltRank.BLACK2;}
+		if (beltType == 12) {beltRank = BeltRank.BLACK3;}
 	}
 	
 	/**
 	 * Writes account info to UserAccounts.txt
 	 */
 	public void write() {
-		String[] arr = {username, password, String.valueOf(acctType.toString())};
+		String[] arr = {username, password, String.valueOf(acctType.toString()),
+				String.valueOf(beltRank.toString())};
 		try {
 			FileWriter fw = new FileWriter(file, true);
 		    BufferedWriter bw = new BufferedWriter(fw);
